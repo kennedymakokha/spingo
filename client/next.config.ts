5e99b62e2d84397d6d62ff4aca10d4e09547377e
kennedymakokha <katchibo2@gmail.com>
2025-03-24 17:05:16 +0300

    fixing the proxy

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-        {
-            source: "/api/:path*",
-            destination: "http://185.113.249.137:4000/api/:path*", // Adjust API server URL
-        },
-    ];
-},
+//   async rewrites() {
+//     return [
+//         {
+//             source: "/api/:path*",
+//             destination: "http://185.113.249.137:4000/api/:path*", // Adjust API server URL
+//         },
+//     ];
+// },
 };
 
 export default nextConfig;
