00d976f64677bc8d26c2cfd44a9afe27fdad1d13
kennedymakokha <katchibo2@gmail.com>
2025-03-24 17:15:28 +0300

    fixing

--- client/components/socket.tsx
+++ client/components/socket.tsx
@@ -2,7 +2,7 @@
 import io from "socket.io-client";
 
 
-export const socket = io("http://185.113.249.137:4000", {
+export const socket = io(`http://localhost:4000`, {
     // autoConnect: false,
     transports: ["websocket", "polling"], // Ensure proper transport
     withCredentials: true, // Allow cross-origin credentials
