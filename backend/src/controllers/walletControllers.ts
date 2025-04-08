3a92b2e860c7a6726935f1d09132aa3b3926f8c1
kennedymakokha <katchibo2@gmail.com>
2025-04-08 20:44:08 +0300

    trying to get Mpesa Callback WIP

--- backend/src/controllers/walletControllers.ts
+++ backend/src/controllers/walletControllers.ts
@@ -119,11 +119,12 @@ export const get_contributions = async (req: Request | any, res: Response | any)
 
 export const mpesa_callback = async (req: Request | any, res: Response | any) => {
     try {
+        console.log("BODY", req.body)
         const Logs = await MpesaLogs.find({
             MerchantRequestID: req.body.Body?.stkCallback?.MerchantRequestID
         })
         console.log("LOGS", Logs)
-        console.log("BODY", req.body)
+        
         for (let i = 0; i < Logs.length; i++) {
 
             await MpesaLogs.findOneAndUpdate(
