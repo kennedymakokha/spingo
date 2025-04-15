8cf9b9dc04c28efa83b91131897ff96644c513d1
kennedymakokha <katchibo2@gmail.com>
2025-04-15 09:06:49 +0300

    fixing the dist folder

--- backend/dist/routes/smsRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const smsEndpoint_1 = require("../controllers/smsEndpoint");
-const router = (0, express_1.Router)();
-router.post("/", smsEndpoint_1.sendSms);
-exports.default = router;
